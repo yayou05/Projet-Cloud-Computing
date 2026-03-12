922b894895c93b6d90468f20e41951fe33a1223b
raïssa <eyram-raissa.mable@efrei.net>
2026-03-12 12:48:09 +0100

    address IP

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "azurerm_network_interface" "projet" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.projet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.projet.id
   }
 }
 
@@ -52,4 +53,67 @@ resource "azurerm_windows_virtual_machine" "projet" {
     sku       = "2016-Datacenter"
     version   = "latest"
   }
+}
+
+
+
+# stocker des fichiers statiques et gérer les permissions pour sécuriser l’accès
+
+resource "azurerm_storage_account" "projet" {
+  name                     = "projetstoragetp1203" 
+  resource_group_name      = azurerm_resource_group.projet.name
+  location                 = azurerm_resource_group.projet.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  allow_nested_items_to_be_public = false
+  min_tls_version                 = "TLS1_2"
+}
+
+resource "azurerm_storage_container" "images" {
+  name                  = "images"
+  storage_account_name  = azurerm_storage_account.projet.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "logs" {
+  name                  = "logs"
+  storage_account_name  = azurerm_storage_account.projet.name
+  container_access_type = "private"
+}
+
+# Déployer un backend
+
+resource "azurerm_public_ip" "projet" {
+  name                = "projet-public-ip"
+  location            = azurerm_resource_group.projet.location
+  resource_group_name = azurerm_resource_group.projet.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "projet" {
+  name                = "projet-nsg"
+  location            = azurerm_resource_group.projet.location
+  resource_group_name = azurerm_resource_group.projet.name
+
+  security_rule {
+    name                       = "NodeJS"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "projet" {
+  network_interface_id      = azurerm_network_interface.projet.id
+  network_security_group_id = azurerm_network_security_group.projet.id
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.projet.ip_address
 }
