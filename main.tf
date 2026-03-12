dbdeea3a170c6d406966a96e4afae1dd530f37cb
raïssa <eyram-raissa.mable@efrei.net>
2026-03-12 15:16:34 +0100

    conection VM et VS code

--- main.tf
+++ main.tf
@@ -97,6 +97,18 @@ resource "azurerm_network_security_group" "projet" {
   resource_group_name = azurerm_resource_group.projet.name
 
   security_rule {
+    name                       = "RDP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
     name                       = "NodeJS"
     priority                   = 1001
     direction                  = "Inbound"
@@ -104,6 +116,18 @@ resource "azurerm_network_security_group" "projet" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
